cfc8858d98ecf28bb011c1401a0c7238b4a486b6
EzeD03 <ezedrojas03@gmail.com>
2022-06-13 17:08:11 -0300

    Server running

--- introExpress/app.js
+++ introExpress/app.js
@@ -4,5 +4,6 @@ const port = 3030
 
 app.get("/", (req,res) => res.send("Bienvenidos a mí servidor."))
 app.get("/contacto", (req,res) => res.send("Mi número es 11-5818-8288."))
+app.get("/bienvenida", (req,res) => res.send("Llegaste bien pedazo de forro y la recalcadaconchadetumadre"))
 
-app.listen(port, () => console.log(`Servidor corriendo en https://localhost:${port}`))
+app.listen(port, () => console.log(`Servidor corriendo en http://localhost:${port}`))
